e4f1163e7d6bb1348ddffb3031cb225e716c5020
SatyamVyas <vyas.satyam.04@gmail.com>
2023-12-23 12:54:56 +0530

    Sorting-I Done

---
+++ 2. Sorting/2.1 Sorting-I/3. InsertionSort.py
@@ -0,0 +1,37 @@
+
+import os
+import sys
+from copy import deepcopy
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+sys.setrecursionlimit(10 ** 7)
+def insertionSort(arr):
+    # write your code here !!!
+    for i in range(1, len(arr)):
+        j = i
+        while j>0 and arr[j-1] > arr[j]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j-=1
+
+        
+class Runner:
+    def __init__(self):
+        self.n = 0
+        self.arr =[]
+
+    def takeInput(self):
+        self.n = int(input())
+        self.arr= list(map(int, input().split()))
+
+    def execute(self):
+
+        ans = insertionSort(self.arr)
+
+    def executeAndPrintOutput(self):
+        ans = insertionSort(self.arr)
+        print(*self.arr)
+
+runner = Runner()
+runner.takeInput()
+runner.executeAndPrintOutput()
+
+# Link: https://www.codingninjas.com/studio/problems/insertion-sort_624381
