6a7756847139156b1773f6044789c974637968d5
SatyamVyas <vyas.satyam.04@gmail.com>
2023-12-30 01:26:55 +0530

    Arrays Hard half done

--- 3. Arrays/3.3. Hard/3. 3Sum.py
+++ 3. Arrays/3.3. Hard/3. 3Sum.py
@@ -42,3 +42,45 @@ def triplet(n: int, nums: [int]) -> [[int]]:
     return list(map(list, res))
 
 # Link: https://www.codingninjas.com/studio/problems/three-sum_6922132
+
+# Optimal Approach
+# def triplet(n, arr):
+#     ans = []
+#     arr.sort()
+#     for i in range(n):
+#         # remove duplicates:
+#         if i != 0 and arr[i] == arr[i - 1]:
+#             continue
+
+#         # moving 2 pointers:
+#         j = i + 1
+#         k = n - 1
+#         while j < k:
+#             total_sum = arr[i] + arr[j] + arr[k]
+#             if total_sum < 0:
+#                 j += 1
+#             elif total_sum > 0:
+#                 k -= 1
+#             else:
+#                 temp = [arr[i], arr[j], arr[k]]
+#                 ans.append(temp)
+#                 j += 1
+#                 k -= 1
+#                 # skip the duplicates:
+#                 while j < k and arr[j] == arr[j - 1]:
+#                     j += 1
+#                 while j < k and arr[k] == arr[k + 1]:
+#                     k -= 1
+
+#     return ans
+
+
+# arr = [-1, 0, 1, 2, -1, -4]
+# n = len(arr)
+# ans = triplet(n, arr)
+# for it in ans:
+#     print("[", end="")
+#     for i in it:
+#         print(i, end=" ")
+#     print("]", end=" ")
+# print()
---
+++ 3. Arrays/3.3. Hard/5. LongestSubArrSum0.py
@@ -0,0 +1,25 @@
+from typing import *
+
+def getLongestZeroSumSubarrayLength(arr : List[int]) -> int:
+    # Write your code here.
+    hashmap = {}
+    s = 0
+    maxlen = 0
+    for i in range(len(arr)):
+        curr = arr[i]
+        s += curr
+        if s == 0:
+            maxlen = i+1
+        
+        rem = 0 - s
+        if rem in hashmap.keys():
+            maxlen = max(maxlen, i - hashmap[rem])
+
+        if s in hashmap.keys():
+            maxlen = max(maxlen, i - hashmap[s]) # Slight Change here
+        else:
+            hashmap[s] = i
+        
+    return maxlen
+
+# Link: https://www.codingninjas.com/studio/problems/longest-subarray-with-zero-sum_6783450
