e39bfb231e7f711003dbda4eb1e9b80f5cb719d0
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-02 19:01:19 +0530

    Rotated Binary Search Done

---
+++ 4. BinarySearch/4.1. BinarySearch1D/8. SearchRotated.py
@@ -0,0 +1,22 @@
+def search(arr, n, k):
+    # Write your code here
+    low = 0
+    high = n - 1
+    ans = 0
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == k:
+            return mid
+        elif arr[low] <= arr[mid]:
+            if arr[low] <= k <= arr[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        else:
+            if arr[mid] <= k <= arr[high]:
+                low = mid + 1    
+            else:
+                high = mid - 1
+    return -1
+
+# Link: https://www.codingninjas.com/studio/problems/search-in-rotated-sorted-array_1082554
---
+++ 4. BinarySearch/4.1. BinarySearch1D/9. SearchRotated2Leet.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, arr: [int], k: int) -> bool:
+        if len(arr) == 1:
+            return arr[0] == k
+        
+        low = 0
+        high = len(arr) - 1
+        ans = 0
+        
+        while low <= high:
+            mid = (low + high)//2
+            if arr[mid] == k or arr[low] == k or arr[high] == k:
+                return True
+            elif arr[low] == arr[mid] == arr[high]:
+                low += 1
+                high -= 1
+            elif arr[low] <= arr[mid]:
+                if arr[low] <= k <= arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if arr[mid] <= k <= arr[high]:
+                    low = mid + 1    
+                else:
+                    high = mid - 1
+        return False
+    
+# Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
