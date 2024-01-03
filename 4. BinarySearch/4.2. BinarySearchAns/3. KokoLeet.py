7248f0c0ad34eb8b1ce6f8ee1a8ae0cd7ab07546
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-03 22:54:30 +0530

    Binary Search on Ans 3/14 Done

---
+++ 4. BinarySearch/4.2. BinarySearchAns/3. KokoLeet.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, v: [int], h: int) -> int:
+        from math import ceil
+        low = 1
+        high = max(v)
+        ans = float("inf")
+        while low <= high:
+            mid = (low + high) // 2
+            totalhrs = 0
+            for i in v:
+                totalhrs += ceil(i/mid)
+            if totalhrs <= h:
+                ans = min(ans, mid)
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+
+# Link: https://leetcode.com/problems/koko-eating-bananas/
