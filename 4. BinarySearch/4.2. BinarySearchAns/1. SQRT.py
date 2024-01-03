b77a713c68f54d5262ce5314994584d84adf9e69
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-03 16:09:05 +0530

    Started BST on Answers

---
+++ 4. BinarySearch/4.2. BinarySearchAns/1. SQRT.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+low = 1
+high = n
+ans = 0
+
+while low <= high:
+   mid = (low + high) // 2
+   if mid**2 == n:
+      ans = mid
+      break
+   elif mid**2 < n:
+      ans = mid
+      low = mid + 1
+   else:
+      high = mid - 1
+
+print(ans)
+
+# Link: https://www.codingninjas.com/studio/problems/square-root-integral_893351
