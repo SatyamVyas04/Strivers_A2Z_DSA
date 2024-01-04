30903e85e5cd9d9804adac44e4eff2cd0d5ef87a
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-04 23:15:30 +0530

    BS on Answers 7/14 done

---
+++ 4. BinarySearch/4.2. BinarySearchAns/5. SmallestDivisor.py
@@ -0,0 +1,23 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def smallestDivisor(arr: [int], limit: int) -> int:
+    # Write your code here.
+    low = 1
+    high = max(arr)
+    ans = float("inf")
+    while low<=high:
+        mid = (low + high) // 2
+        currsum = 0
+        for i in arr:
+            currsum += ceil(i/mid)
+        if currsum <= limit:
+            ans = min(ans, mid)
+            high = mid - 1
+        else:
+            low = mid + 1
+    return ans
+
+# Link: https://www.codingninjas.com/studio/problems/smallest-divisor-with-the-given-limit_1755882
---
+++ 4. BinarySearch/4.2. BinarySearchAns/6. ShipCap.py
@@ -0,0 +1,29 @@
+def findDays(weights, cap):
+    days = 1  # First day
+    load = 0
+    n = len(weights)  # Size of array
+    for i in range(n):
+        if load + weights[i] > cap:
+            days += 1  # Move to next day
+            load = weights[i]  # Load the weight
+        else:
+            # Load the weight on the same day
+            load += weights[i]
+    return days
+
+def leastWeightCapacity(weights, d):
+    # Find the maximum and the summation
+    low = max(weights)
+    high = sum(weights)
+    while low <= high:
+        mid = (low + high) // 2
+        numberOfDays = findDays(weights, mid)
+        if numberOfDays <= d:
+            # Eliminate right half
+            high = mid - 1
+        else:
+            # Eliminate left half
+            low = mid + 1
+    return low
+
+# Link: https://www.codingninjas.com/studio/problems/capacity-to-ship-packages-within-d-days_1229379
