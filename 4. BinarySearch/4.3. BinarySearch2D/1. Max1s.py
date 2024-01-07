bbdd821a30d34cd5b233d34b2d936b9f3e604613
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-07 19:03:01 +0530

    Started 2D BST

---
+++ 4. BinarySearch/4.3. BinarySearch2D/1. Max1s.py
@@ -0,0 +1,16 @@
+def rowWithMax1s(matrix: [[int]], n: int, m: int) -> int:
+    maxCount = 0
+    ans = -1
+
+    for i in range(n): 
+        curr = 0
+        if 1 in matrix[i]:
+            curr = m - matrix[i].index(1)
+
+        if curr > maxCount:
+            ans = i
+            maxCount = curr
+
+    return ans
+
+# Link: https://www.codingninjas.com/studio/problems/row-of-a-matrix-with-maximum-ones_982768
---
+++ 4. BinarySearch/4.3. BinarySearch2D/2. TargetSearch.py
@@ -0,0 +1,23 @@
+def search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low<=high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            return 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 0
+
+
+def searchMatrix(mat: [[int]], target: int) -> bool:
+    # Write your code here.
+    for i in range(len(mat)):
+        x = search(mat[i], target)
+        if x == 1:
+            return True
+    return False
+
+# Link: https://www.codingninjas.com/studio/problems/search-in-a-2d-matrix_980531
