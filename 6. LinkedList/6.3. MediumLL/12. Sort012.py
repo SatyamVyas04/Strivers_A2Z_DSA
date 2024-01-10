a13776c2e1be4d1b6a526170a459f40b4ea50258
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-10 23:26:35 +0530

    12/15 Done

---
+++ 6. LinkedList/6.3. MediumLL/12. Sort012.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def sortList(head):
+    # Write your code here
+    temp = head
+    if not head or not head.next:
+        return head
+
+    a = zero = Node(-1)
+    b = ones = Node(-1)
+    c = twos = Node(-1)
+
+    while temp:
+        if temp.data == 0:
+            a.next = Node(0)
+            a = a.next
+        elif temp.data == 1:
+            b.next = Node(1)
+            b = b.next
+        else:
+            c.next = Node(2)
+            c = c.next   
+        temp = temp.next
+    
+    if zero.next:
+        zero = zero.next
+    if ones.next:
+        ones = ones.next
+    if twos.next:
+        twos = twos.next
+    
+    a.next = ones
+    b.next = twos
+    return zero
+
+# Link: https://www.codingninjas.com/studio/problems/sort-linked-list-of-0s-1s-2s_1071937
---
+++ 6. LinkedList/6.3. MediumLL/8. SegregateLeet.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or head.next == None:
+            return head
+        temp = head
+        evenstart = head.next
+        fwd = None
+        cnt = 0
+        while temp.next.next:
+            cnt += 1
+            fwd = temp.next
+            temp.next = temp.next.next
+            temp = fwd
+        if cnt%2 == 0:
+            # temp == last odd
+            # temp.next == last even
+            # [7]->[8]->None
+            temp.next = evenstart
+        else:
+            # temp == last even
+            # temp.next == last odd
+            # [8]->[9]->None
+            temp.next.next = evenstart
+            temp.next = None
+        return head
+    
+# Link: https://leetcode.com/problems/odd-even-linked-list/
