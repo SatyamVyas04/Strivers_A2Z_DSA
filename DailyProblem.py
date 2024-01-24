282365495483cc753f68f7fe0fa217264b5e2f7b
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-24 18:01:48 +0530

    DailyProblem Done

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,15 +1,31 @@
-class Solution:
-    def maxLength(self, arr: list[str]) -> int:
-        dp = [set()]
-        for word in arr:
-            letters = set(word)
-            if len(letters) != len(word):
-                continue
-            for soln in dp:
-                if soln & letters:
-                    continue
-                dp.append(soln | letters)
-        return len(max(dp, key=len))
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-sol = Solution()
-print(sol.maxLength(["ab","cd","cde","cdef","efg","fgh","abxyz"]))
+
+class Solution:
+    def pseudoPalindromicPaths(self, root: list[TreeNode]) -> int:
+        paths = []
+        ans = 0
+
+        def dfs(node, currpath):
+            if not node:
+                return 0
+
+            if node.val in currpath:
+                currpath.remove(node.val)
+            else:
+                currpath.add(node.val)
+
+            if not node.left and not node.right:
+                return 1 if len(currpath) <= 1 else 0
+
+            left = dfs(node.left, set(currpath))
+            right = dfs(node.right, set(currpath))
+
+            return left + right
+
+        return dfs(root, set())
