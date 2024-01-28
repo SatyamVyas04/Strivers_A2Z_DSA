b4facb4ac35ae59987cf22d40ce88efb32cc672d
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-28 23:07:57 +0530

    Started Bit Manipulation

---
+++ 8. BitManipulation/7.1. Basics/1. FindSetUnset.py
@@ -0,0 +1,6 @@
+def bitManipulation(num: int, i: int) -> list[int]:
+    # Write your code here.
+    ans1 = num >> (i-1) & 1
+    ans2 = num | 1 << i-1
+    ans3 = num & ~(1 << i-1)
+    return [ans1, ans2, ans3]
