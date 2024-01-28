d39ab3dbefc181c24593881b61afdecf7025380e
SatyamVyas <vyas.satyam.04@gmail.com>
2024-01-28 18:25:39 +0530

    Daily Hard Problem Understood :)

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,16 +1,21 @@
 class Solution:
-    def kInversePairs(self, n: int, k: int) -> int:
-        m = 10**9+7
-        prev = [0] * (k+1)
-        prev[0] = 1
+    def numSubmatrixSumTarget(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
 
-        for N in range(1, n+1):
-            curr = [0] * (k+1)
-            total = 0  # Sliding Window
-            for K in range(k+1):
-                if K >= N:
-                    total -= prev[K-N]
-                total += prev[K]
-                curr[K] = total % m
-            prev = curr
-        return prev[k]
+        for row in range(m):
+            for col in range(1, n):
+                matrix[row][col] += matrix[row][col - 1]
+
+        count = 0
+
+        for c1 in range(n):
+            for c2 in range(c1, n):
+                prefix_sum_count = {0: 1}
+                sum_val = 0
+
+                for row in range(m):
+                    sum_val += matrix[row][c2] - (matrix[row][c1 - 1] if c1 > 0 else 0)
+                    count += prefix_sum_count.get(sum_val - target, 0)
+                    prefix_sum_count[sum_val] = prefix_sum_count.get(sum_val, 0) + 1
+
+        return count
