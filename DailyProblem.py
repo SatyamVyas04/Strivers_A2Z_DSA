ccb73b6ff219dfdd4267588128782878bf2f4eb8
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-01-29 09:14:46 +0530

    Update DailyProblem.py

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,21 +1,25 @@
-class Solution:
-    def numSubmatrixSumTarget(self, matrix, target):
-        m, n = len(matrix), len(matrix[0])
+class MyQueue:
+    def __init__(self):
+        self.arr = []
 
-        for row in range(m):
-            for col in range(1, n):
-                matrix[row][col] += matrix[row][col - 1]
+    def push(self, x: int) -> None:
+        self.arr.append(x)
 
-        count = 0
+    def pop(self) -> int:
+        return self.arr.pop(0)
 
-        for c1 in range(n):
-            for c2 in range(c1, n):
-                prefix_sum_count = {0: 1}
-                sum_val = 0
+    def peek(self) -> int:
+        return self.arr[0]
 
-                for row in range(m):
-                    sum_val += matrix[row][c2] - (matrix[row][c1 - 1] if c1 > 0 else 0)
-                    count += prefix_sum_count.get(sum_val - target, 0)
-                    prefix_sum_count[sum_val] = prefix_sum_count.get(sum_val, 0) + 1
+    def empty(self) -> bool:
+        if not self.arr:
+            return True
+        return False
 
-        return count
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
