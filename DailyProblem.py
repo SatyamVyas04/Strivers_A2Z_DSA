c197e72142544090d14f216081b14a77bebd15dd
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-02-02 11:38:28 +0530

    Update DailyProblem.py

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,25 +1,8 @@
-class MyQueue:
-    def __init__(self):
-        self.arr = []
-
-    def push(self, x: int) -> None:
-        self.arr.append(x)
-
-    def pop(self) -> int:
-        return self.arr.pop(0)
-
-    def peek(self) -> int:
-        return self.arr[0]
-
-    def empty(self) -> bool:
-        if not self.arr:
-            return True
-        return False
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+ans = []
+for i in range(1, 9):
+    x = i
+    for j in range(i+1, 10):
+        x = x*10 + j
+        if low <= j <= high:
+            ans.append(x)
+return sorted(ans)
