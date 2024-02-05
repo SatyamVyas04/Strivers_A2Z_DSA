7a741b581fa879218c34edd42e72993a635cd69e
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-05 13:57:32 +0530

    Daily Problem Done :)

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,8 +1,7 @@
-ans = []
-for i in range(1, 9):
-    x = i
-    for j in range(i+1, 10):
-        x = x*10 + j
-        if low <= j <= high:
-            ans.append(x)
-return sorted(ans)
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        c = dict(Counter(s))
+        for i in range(len(s)):
+            if c[s[i]] == 1:
+                return i
+        return -1
