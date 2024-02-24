b8971ad8c187684ae5c9569fefc0569be6ff62fd
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-24 12:29:12 +0530

    Completed Daily Problem

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,7 +1,33 @@
+from collections import defaultdict
+
+
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        c = dict(Counter(s))
-        for i in range(len(s)):
-            if c[s[i]] == 1:
-                return i
-        return -1
+    def findAllPeople(self, n: int, meetings: list[list[int]], firstPerson: int) -> list[int]:
+        secrets = {0, firstPerson}
+        time_map = {}
+
+        for src, dst, time in meetings:
+            if time not in time_map:
+                time_map[time] = defaultdict(list[int])
+            time_map[time][src].append(dst)
+            time_map[time][dst].append(src)
+
+        def dfs(src, adj):
+            if src in visited:
+                return
+            visited.add(src)
+            secrets.add(src)
+            for nei in adj[src]:
+                dfs(nei, adj)
+
+        for time in sorted(time_map.keys()):
+            visited = set()
+            for src in time_map[time]:
+                if src in secrets:
+                    dfs(src, time_map[time])
+
+        return list(secrets)
+
+
+s = Solution()
+print(s.findAllPeople(6, [[1, 2, 5], [2, 3, 8], [1, 5, 10]], 1))
