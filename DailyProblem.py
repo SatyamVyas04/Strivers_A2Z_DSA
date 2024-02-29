13cc739654149383838feac3d01385f0b842f05b
SatyamVyas <vyas.satyam.04@gmail.com>
2024-02-29 10:40:26 +0530

    Solved the Daily Problem

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,33 +1,57 @@
-from collections import defaultdict
+from collections import deque
+from sys import maxsize
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def findAllPeople(self, n: int, meetings: list[list[int]], firstPerson: int) -> list[int]:
-        secrets = {0, firstPerson}
-        time_map = {}
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        # Create a queue for nodes that need to be visited and add the root
+        queue = deque()
+        current = root
+        queue.append(current)
 
-        for src, dst, time in meetings:
-            if time not in time_map:
-                time_map[time] = defaultdict(list[int])
-            time_map[time][src].append(dst)
-            time_map[time][dst].append(src)
+        # Keeps track of whether we are on an even level
+        even = True
 
-        def dfs(src, adj):
-            if src in visited:
-                return
-            visited.add(src)
-            secrets.add(src)
-            for nei in adj[src]:
-                dfs(nei, adj)
+        # While there are more nodes in the queue
+        # Determine the size of the level and handle the nodes
+        while queue:
+            size = len(queue)
 
-        for time in sorted(time_map.keys()):
-            visited = set()
-            for src in time_map[time]:
-                if src in secrets:
-                    dfs(src, time_map[time])
+            # Prev holds the value of the previous node in this level
+            prev = maxsize
+            if even:
+                prev = -maxsize
 
-        return list(secrets)
+            # While there are more nodes in this level
+            # Remove a node, check whether it satisfies the conditions
+            # Add its children to the queue
+            while size > 0:
+                current = queue.popleft()
 
+                # If on an even level, check that the node's value is odd and greater than prev
+                # If on an odd level, check that the node's value is even and less than prev
+                if (even and (current.val % 2 == 0 or current.val <= prev)) or \
+                        (not even and (current.val % 2 == 1 or current.val >= prev)):
+                    return False
 
-s = Solution()
-print(s.findAllPeople(6, [[1, 2, 5], [2, 3, 8], [1, 5, 10]], 1))
+                prev = current.val
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+                # Decrement size, we have handled a node on this level
+                size -= 1
+
+            # Flip the value of even, the next level will be opposite
+            even = not even
+
+        # If every node meets the conditions, the tree is Even-Odd
+        return True
