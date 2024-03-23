88f8a1e78176670166ac5b5d3815862694c8f89b
SatyamVyas <vyas.satyam.04@gmail.com>
2024-03-23 12:53:39 +0530

    Solved Daily Challenge

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,12 +1,26 @@
 class Solution:
-    def minimumLength(self, s: str) -> int:
-        left, right = 0, len(s) - 1
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # find middle
+        if not head:
+            return []
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        while left < right and s[left] == s[right]:
-            char = s[left]
-            while left <= right and s[left] == char:
-                left += 1
-            while right >= left and s[right] == char:
-                right -= 1
+        # reverse other second half
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        slow.next = None
 
-        return right - left + 1
+        # merging
+        head1, head2 = head, prev
+        while head2:
+            nextt = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = nextt
