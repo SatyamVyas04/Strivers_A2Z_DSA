5429a92d5134aeca3562747684e9b72d4e729c3b
SatyamVyas <vyas.satyam.04@gmail.com>
2024-04-18 13:44:09 +0530

    Completed Daily Problem

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,27 +1,24 @@
+# Island Perimeter
 class Solution:
-    def maximalRectangle(self, matrix: list[list[str]]) -> int:
+    def islandPerimeter(self, grid: list[list[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
         ans = 0
-        heights = [0]*len(matrix[0])
-        for row in range(len(matrix)):
-            for col in range(len(matrix[0])):
-                if matrix[row][col] == "1":
-                    heights[col] += int(matrix[row][col])
-                else:
-                    heights[col] = 0
-            ans = max(ans, self.findTallestRect(heights))
-        return ans
-
-    def findTallestRect(self, heights: list[int]) -> int:
-        ans = 0
-        stack = []
-        for i in range(len(heights)+1):
-            while stack and ((i == len(heights)) or (heights[stack[-1]] >= heights[i])):
-                height = heights[stack[-1]]
-                stack.pop()
-                if not stack:
-                    width = i
-                else:
-                    width = i - stack[-1]-1
-                ans = max(ans, width*height)
-            stack.append(i)
+        for row in range(rows):
+            for col in range(cols):
+                curr = grid[row][col]
+                if curr:
+                    ans += 4
+                    if row > 0:
+                        if grid[row - 1][col]:
+                            ans -= 1
+                    if row < rows - 1:
+                        if grid[row + 1][col]:
+                            ans -= 1
+                    if col > 0:
+                        if grid[row][col - 1]:
+                            ans -= 1
+                    if col < cols - 1:
+                        if grid[row][col + 1]:
+                            ans -= 1
         return ans
