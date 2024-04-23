e60458d17a4ede02b1e8c30b59ab12f5f483722d
SatyamVyas <vyas.satyam.04@gmail.com>
2024-04-23 09:34:48 +0530

    Completed Daily Problem

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,24 +1,33 @@
-# Island Perimeter
+from collections import defaultdict
+
+
 class Solution:
-    def islandPerimeter(self, grid: list[list[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        ans = 0
-        for row in range(rows):
-            for col in range(cols):
-                curr = grid[row][col]
-                if curr:
-                    ans += 4
-                    if row > 0:
-                        if grid[row - 1][col]:
-                            ans -= 1
-                    if row < rows - 1:
-                        if grid[row + 1][col]:
-                            ans -= 1
-                    if col > 0:
-                        if grid[row][col - 1]:
-                            ans -= 1
-                    if col < cols - 1:
-                        if grid[row][col + 1]:
-                            ans -= 1
-        return ans
+    def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
+        total_node_count = n
+        if total_node_count == 1:
+            # Quick response for one node tree
+            return [0]
+
+        # build adjacency matrix
+        adj_matrix = defaultdict(set)
+        for src_node, dst_node in edges:
+            adj_matrix[src_node].add(dst_node)
+            adj_matrix[dst_node].add(src_node)
+
+        # get leaves node whose degree is 1
+        leave_nodes = [node for node in adj_matrix if len(adj_matrix[node]) == 1]
+
+        # keep doing leave nodes removal until total node count is smaller or equal to 2
+        while total_node_count > 2:
+            total_node_count -= len(leave_nodes)
+            leave_nodes_next_round = []
+            # leave nodes removal
+            for leaf in leave_nodes:
+                neighbor = adj_matrix[leaf].pop()
+                adj_matrix[neighbor].remove(leaf)
+                if len(adj_matrix[neighbor]) == 1:
+                    leave_nodes_next_round.append(neighbor)
+            leave_nodes = leave_nodes_next_round
+
+        # final leave nodes are root node of minimum height trees
+        return leave_nodes
