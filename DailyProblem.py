f77845bea8f4f12e8beb64215bfbcd036c66913c
SatyamVyas <vyas.satyam.04@gmail.com>
2024-05-31 12:20:02 +0530

    5/5 Done

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,14 +1,17 @@
-from collections import defaultdict
+from functools import reduce
+from operator import xor
 
 
 class Solution:
-    def countTriplets(self, arr: list[int]) -> int:
-        ans = 0
-        n = len(arr)
-        for i in range(n-1):
-            cur_xor = arr[i]
-            for k in range(i+1, n):
-                cur_xor ^= arr[k]
-                if cur_xor == 0:
-                    ans += k - i
-        return ans
+    def singleNumber(self, nums):
+        s = reduce(xor, nums)
+
+        nz = s & (s-1) ^ s
+
+        num1 = reduce(xor, filter(lambda n: n & nz, nums))
+
+        return (num1, s ^ num1)
+
+
+sol = Solution()
+print(sol.singleNumber([1, 1, 2, 2, 3, 5]))
