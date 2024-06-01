32ef58b347201602e5da76959ecf943e05cba6dd
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-01 13:12:55 +0530

    9.3 Started

---
+++ 9. Stack&Queue/9.3. Monotonic/NextGreaterElementLeet.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        index_map = {num: idx for idx, num in enumerate(nums1)}
+        result = [-1] * len(nums1)
+        stack = []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller_num = stack.pop()
+                result[index_map[smaller_num]] = num
+            if num in index_map:
+                stack.append(num)
+
+        return result
