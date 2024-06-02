1d0d5535dee40046b86c9222bb7a3cf42b842dfa
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-02 16:38:05 +0530

    Completed 9.3

---
+++ 9. Stack&Queue/9.3. Monotonic/10. LargestRectLeet.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        ans = 0
+        stack = []
+        for i in range(len(heights) + 1):
+            while stack and ((i == len(heights)) or (heights[stack[-1]] >= heights[i])):
+                height = heights[stack[-1]]
+                stack.pop()
+                if not stack:
+                    width = i
+                else:
+                    width = i - stack[-1] - 1
+                ans = max(ans, width * height)
+            stack.append(i)
+        return ans
+
+# Link: https://leetcode.com/problems/largest-rectangle-in-histogram/
