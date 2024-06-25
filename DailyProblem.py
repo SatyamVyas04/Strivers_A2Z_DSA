2a91eccb8f33ff1abe086f0f9cc398a01fcff123
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-25 12:16:09 +0530

    Daily Problem Solved

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,57 +1,29 @@
-# from sortedcontainers import SortedList
-
-
-# class Solution:
-#     def longestSubarray(self, nums: list[int], limit: int) -> int:
-#         ans = 0
-#         n = len(nums)
-#         sl = SortedList()
-
-#         l = 0
-#         for r in range(n):
-#             sl.add(nums[r])
-#             while sl[-1] - sl[0] > limit:  # type: ignore
-#                 sl.remove(nums[l])
-#                 l += 1
-#             ans = max(ans, r - l + 1)
-
-#         return ans
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
-    def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_deque = deque()
-        min_deque = deque()
-        left = 0
-        max_length = 0
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        nodes = []
+        dummy = TreeNode(-1)
+        temp = dummy
 
-        for right in range(len(nums)):
-            # Maintain the max_deque in decreasing order
-            while max_deque and max_deque[-1] < nums[right]:
-                max_deque.pop()
-            max_deque.append(nums[right])
+        def dfs(root, temp):
+            if root:
+                if root.right:
+                    temp.right = TreeNode()
+                    dfs(root.right, temp.right)
+                nodes.append(root.val)
+                temp.val = sum(nodes)
+                if root.left:
+                    temp.left = TreeNode()
+                    dfs(root.left, temp.left)
 
-            # Maintain the min_deque in increasing order
-            while min_deque and min_deque[-1] > nums[right]:
-                min_deque.pop()
-            min_deque.append(nums[right])
+        dfs(root, temp)
+        return dummy
 
-            # Check if the current window exceeds the limit
-            while max_deque[0] - min_deque[0] > limit:
-                # Remove the elements that are out of the current window
-                if max_deque[0] == nums[left]:
-                    max_deque.popleft()
-                if min_deque[0] == nums[left]:
-                    min_deque.popleft()
-                left += 1
-
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
-
-
-sol = Solution()
-print(sol.longestSubarray(nums=[8, 2, 4, 7], limit=4))
-print(sol.longestSubarray(nums=[10, 1, 2, 4, 7, 2], limit=5))
-print(sol.longestSubarray(nums=[4, 2, 2, 2, 4, 4, 2, 2], limit=0))
-print(sol.longestSubarray(nums=[1, 5, 6, 7, 8, 10, 6, 5, 6], limit=4))
+# Link: https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
