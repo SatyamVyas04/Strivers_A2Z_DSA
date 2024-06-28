e2b83ede59fcc0331a53f2edf13627b4e67ee211
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-28 23:14:15 +0530

    Medium 8/12

---
+++ 13. BinaryTrees/13.2. Medium/5. SameTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSameTree(self, p, q):
+        # If both nodes are None, they are identical
+        if p is None and q is None:
+            return True
+        # If only one of the nodes is None, they are not identical
+        if p is None or q is None:
+            return False
+        # Check if values are equal and recursively check left and right subtrees
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # Values are not equal, they are not identical
+        return False
+    
+# Link: https://leetcode.com/problems/same-tree/submissions/1303121852/
---
+++ 13. BinaryTrees/13.2. Medium/6. ZigZag.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+from typing import Optional
+
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]: # type: ignore
+        if not root:
+            return []
+        
+        q = deque([(root, 0)])
+        curr_arr = []
+        finalarr = []
+        curr_level = 0
+        booleanFlag = True
+
+        while q:
+            curr, l = q.popleft()
+            if l > curr_level and booleanFlag:
+                finalarr.append(curr_arr)
+                curr_arr = []
+                curr_level = l
+                booleanFlag = False
+            elif l > curr_level and not booleanFlag:
+                finalarr.append(curr_arr[::-1])
+                curr_arr = []
+                curr_level = l
+                booleanFlag = True
+
+            curr_arr.append(curr.val)
+            if curr.left:
+                q.append((curr.left, l + 1)) # type: ignore
+            if curr.right:
+                q.append((curr.right, l + 1)) # type: ignore
+        
+        if curr_arr and booleanFlag:
+            finalarr.append(curr_arr)
+            booleanFlag = False
+        elif curr_arr and not booleanFlag:
+            finalarr.append(curr_arr[::-1])
+            booleanFlag = True
+        
+        return finalarr
+
+# Link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
