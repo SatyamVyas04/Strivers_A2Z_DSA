bd444bc810483d1f21f1e2b667d3d61c643cac56
SatyamVyas <vyas.satyam.04@gmail.com>
2024-06-28 15:48:51 +0530

    Binary Tree Traversals completed

---
+++ 13. BinaryTrees/13.1. Traversals/11. Post2Stack.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
+        if not root:
+            return []
+        stack1 = [root]
+        stack2 = []
+
+        while stack1:
+            curr = stack1.pop()
+            if not curr:
+                continue
+            stack1.append(curr.left)
+            stack1.append(curr.right)
+            stack2.append(curr.val)
+        
+        return stack2[::-1]
+    
+# Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
---
+++ 13. BinaryTrees/13.1. Traversals/9. IterPre.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
+        ret = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                ret.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return ret
+
+# Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
