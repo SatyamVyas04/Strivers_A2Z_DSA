b0a22dfb773af83d29a187701dabf524d7014458
SatyamVyas <vyas.satyam.04@gmail.com>
2024-07-29 11:24:07 +0530

    solved daily problem

--- DailyProblem.py
+++ DailyProblem.py
@@ -1,12 +1,30 @@
 class Solution:
-    def reverseParentheses(self, s: str) -> str:
-        stack = ['']
-        for c in s:
-            if c == '(':
-                stack.append('')
-            elif c == ')':
-                add = stack.pop()[::-1]
-                stack[-1] += add
-            else:
-                stack[-1] += c
-        return stack.pop()
+    def numTeams(self, rating) -> int:
+        n = len(rating)
+        ans = 0
+        # ascend
+        for i in range(1, n-1):
+            cleft, cright = 0, 0
+            for j in range(0, i):
+                if rating[j] < rating[i]:
+                    cleft += 1
+            for j in range(i+1, n):
+                if rating[i] < rating[j]:
+                    cright += 1
+            ans += cleft * cright
+
+        # descend
+        for i in range(1, n-1):
+            cleft, cright = 0, 0
+            for j in range(0, i):
+                if rating[j] > rating[i]:
+                    cleft += 1
+            for j in range(i+1, n):
+                if rating[i] > rating[j]:
+                    cright += 1
+            ans += cleft * cright
+        return ans
+
+
+sol = Solution()
+print(sol.numTeams([2, 5, 3, 4, 1]))
